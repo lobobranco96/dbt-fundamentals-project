f6baef4bc833ed53cd1a4f4f5e19cdd7b4f1a59e
Renato Mariano <rntt2511@gmail.com>
2026-06-15 18:11:23 +0000

    commit

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -13,7 +13,7 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-        amount,
+        amount / 100 as amount,
         created as created_at
 
     from source
